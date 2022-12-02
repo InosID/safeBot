14909e61d9cc05bde3957f1acb4647a762056b43
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-03 05:59:12 +0700

    Update serialize.js

--- app/lib/serialize.js
+++ app/lib/serialize.js
@@ -102,4 +102,360 @@ async function serialize(msg, conn) {
       data,
     }
   }
-  
+  conn.getName = (jid, withoutContact = false) => {
+    id = conn.decodeJid(jid);
+    withoutContact = conn.withoutContact || withoutContact;
+    let v;
+    if (id.endsWith("@g.us"))
+    return new Promise(async (resolve) => {
+      v = store.contacts[id] || {};
+      if (!(v.name || v.subject)) v = conn.groupMetadata(id) || {};
+      resolve(
+        v.name ||
+        v.subject ||
+	require("awesome-phonenumber")("+" + id.replace("@s.whatsapp.net", "")).getNumber(
+	  "international"
+        )
+      );
+    });
+    else
+      v = id === "0@s.whatsapp.net"
+        ? {
+	  id,
+	  name: "WhatsApp",
+        }
+      : id === conn.decodeJid(conn.user.id)
+      ? conn.user
+      : store.contacts[id] || {};
+      return (
+        (withoutContact ? "" : v.name) ||
+        v.subject ||
+        v.verifiedName ||
+        require("awesome-phonenumber")("+" + jid.replace("@s.whatsapp.net", "")).getNumber("international")
+      )
+  }
+  conn.getBuffer = async (url, options) => {
+    try {
+      options ? options : {};
+      const res = await require("axios") ({
+	method: "get",
+	url,
+	headers: {
+	  DNT: 1,
+	  "Upgrade-Insecure-Request": 1,
+        },
+        ...options,
+        responseType: "arraybuffer",
+      });
+      return res.data;
+    } catch (e) {
+	console.log(`Error : ${e}`);
+    }
+  }
+  conn.sendContact = async (jid, contact, quoted = false, opts = {}) => {
+    let list = [];
+    for (let i of contact) {
+      num = typeof i == "number" ? i + "@s.whatsapp.net" : i;
+      num2 = typeof i == "number" ? i : i.split("@")[0];
+      list.push({
+	displayName: await conn.getName(num),
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${await conn.getName(num)}\nFN:${await conn.getName(
+	  num
+	)}\nitem1.TEL;waid=${num2}:${num2}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:${
+	  config.email
+        }\nitem2.X-ABLabel:Email\nitem3.URL:${
+	  config.instagram
+        }\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+      });
+    }
+    return conn.sendMessage(
+      jid,
+      { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts },
+      { quoted }
+    )
+  }
+  conn.sendSticker = async (jid, url, quoted, option = {}) => {
+    let ext;
+    let buf = url;
+    if (!Buffer.isBuffer(url)) buf = await conn.getBuffer(url);
+    if (!Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    if (Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    url = ext == "mp4"
+      ? await convert2(buf, ext.ext, "webp", cmd[parseInt(option.cmdType ? option.cmdType : 1)])
+      : await convert(buf, ext.ext, "webp", cmd[parseInt(option.cmdType ? option.cmdType : 1)]);
+    let sticker = { url };
+    console.log(url);
+    return conn.sendMessage(jid, { sticker: url, ...option }, { quoted });
+  }
+  conn.logger = {
+    ...conn.logger,
+    info(...args) {
+      console.log(
+	chalk.bold.rgb(
+	  57,
+	  183,
+	  16
+        )(`INFO [${chalk.rgb(255, 255, 255)(moment(Date.now()).format(" dddd, DD MMMM YYYY HH:mm:ss "))}]`),
+	chalk.cyan(...args)
+      )
+    },
+    error(...args) {
+      console.log(
+	chalk.bold.rgb(
+	  247,
+	  38,
+	  33
+	)(`ERROR [${chalk.rgb(255, 255, 255)(moment(Date.now()).format(" dddd, DD MMMM YYYY HH:mm:ss "))}]:`),
+	chalk.rgb(255, 38, 0)(...args)
+      )
+    },
+    warn(...args) {
+      console.log(
+	chalk.bold.rgb(
+	  239,
+	  225,
+	  3
+	)(`WARNING [${chalk.rgb(255, 255, 255)(moment(Date.now()).format(" dddd, DD MMMM YYYY HH:mm:ss "))}]:`),
+	chalk.keyword("orange")(...args)
+      )
+    },
+  }
+
+  conn.sendGroupV4Invite = async (
+    jid,
+    participant,
+    inviteCode,
+    inviteExpiration,
+    groupName = "unknown subject",
+    jpegThumbnail,
+    caption = "Invitation to join my WhatsApp group",
+    options = {}
+  ) => {
+    let msg = Baileys.proto.Message.fromObject({
+      groupInviteMessage: Baileys.proto.GroupInviteMessage.fromObject({
+	inviteCode,
+	inviteExpiration: inviteExpiration ? parseInt(inviteExpiration) : +new Date(new Date() + 3 * 86400000),
+	groupJid: jid,
+	groupName: groupName ? groupName : (await conn.groupMetadata(jid)).subject,
+	jpegThumbnail,
+	caption,
+      }),
+    });
+    const ms = Baileys.generateWAMessageFromContent(participant, msg, options);
+    await conn.relayMessage(participant, ms.message, { messageId: ms.key.id });
+  }
+  conn.sendImage = async (jid, url, quoted, option = {}) => {
+    let ext;
+    let buf = url;
+    if (!Buffer.isBuffer(url)) buf = await conn.getBuffer(url);
+    if (!Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    if (Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    let type = /jpg|png|webp/i.test(ext.ext);
+    if (!type) return ReferenceError(`Format file invalid`);
+    url = buf;
+    return conn.sendMessage(jid, { image: url, ...option }, { quoted });
+  };
+  conn.sendVideo = async (jid, url, quoted, option = {}) => {
+    let ext;
+    let buf = url;
+    if (!Buffer.isBuffer(url)) buf = await conn.getBuffer(url);
+    if (!Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    if (Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    let type = /gif|webm|mp4/i.test(ext.ext);
+    if (!type) return ReferenceError(`Format file invalid`);
+    url = ext.ext !== "mp4"
+      ? await convert(buf, ext.ext, "mp4", cmd[parseInt(option.cmdType ? option.cmdType : 1)])
+      : buf;
+    return conn.sendMessage(jid, { video: url, ...option, mimetype: ext.mimetype }, { quoted });
+  };
+  conn.sendPoll = async (jid, name = '', optiPoll, options) => {
+    if (!Array.isArray(optiPoll[0]) && typeof optiPoll[0] === 'string') optiPoll = [optiPoll]
+    if (!options) options = {}
+    const pollMessage = {
+      name: name,
+      options: optiPoll.map(btn => ({
+	optionName: btn[0] || ''
+      })),
+      selectableOptionsCount: 1
+    }
+    return conn.relayMessage(jid, { pollCreationMessage: pollMessage }, { ...options });
+  }     
+  conn.sendAudio = async (jid, url, quoted, ptt = false, option = {}) => {
+    let ext;
+    let buf = url;
+    if (!Buffer.isBuffer(url)) buf = await conn.getBuffer(url);
+    if (!Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    if (Buffer.isBuffer(url)) ext = await fromBuffer(buf);
+    let type = /mp3|wav|opus|m4a/i.test(ext.ext);
+    if (!type) return ReferenceError(`Format file invalid`);
+    url = ext.ext !== "mp3"
+      ? await convert(buf, ext.ext, "mp3", cmd[parseInt(option.cmdType ? option.cmdType : 1)])
+      : buf;
+      return conn.sendFile(msg.from, url, Date.now() / 1000 + ext.ext, "", quoted, ptt);
+  }
+  conn.sendFile = async (jid, path, filename = "", caption = "", quoted, ptt = false, options = {}) => {
+    let type = await conn.getFile(path, true);
+    let { res, data: file, filename: pathFile } = type;
+    if ((res && res.status !== 200) || file.length <= 65536) {
+      try {
+	throw { json: JSON.parse(file.toString()) };
+      } catch (e) {
+	if (e.json) throw e.json;
+      }
+    }
+    let opt = { filename };
+    if (quoted) opt.quoted = quoted;
+    if (!type) if (options.asDocument) options.asDocument = true;
+    let mtype = "",
+    mimetype = type.mime;
+    let naem = (a) => "./temp/" + Date.now() + "." + a;
+    if (/webp/.test(type.mime)) mtype = "sticker";
+    else if (/image/.test(type.mime)) mtype = "image";
+    else if (/video/.test(type.mime)) mtype = "video";
+    else if (/audio/.test(type.mime))
+    (ss = await (ptt ? toPTT : toAudio2)(file, type.ext)),
+    (skk = await require("file-type").fromBuffer(ss.data)),
+    (ty = naem(skk.ext)),
+    require("fs").writeFileSync(ty, ss.data),
+    (pathFile = ty),
+    (mtype = "audio"),
+    (mimetype = "audio/mpeg");
+    else mtype = "document";
+    conn.sendMessage(jid, {
+      ...options,
+      caption,
+      ptt,
+      fileName: filename,
+      [mtype]: { url: pathFile },
+      mimetype,
+    }, {
+      ...opt,
+      ...options,
+    }).then(() => {
+      fs.unlinkSync(pathFile);
+      conn.logger.info("delete file " + pathFile);
+    })
+  }
+  if (msg.key) {
+    msg.id = msg.key.id;
+    msg.isSelf = msg.key.fromMe;
+    msg.from = msg.key.remoteJid;
+    msg.isGroup = msg.from.endsWith("@g.us");
+    msg.sender = msg.isGroup
+    ? conn.decodeJid(msg.key.participant)
+    : msg.isSelf
+    ? conn.decodeJid(conn.user.id)
+    : msg.from;
+  }
+  if (msg.message) {
+    msg.type = getContentType(msg.message);
+    if (msg.type === "ephemeralMessage") {
+      msg.message = msg.message[msg.type].message;
+      const tipe = Object.keys(msg.message)[0];
+      msg.type = tipe;
+      if (tipe === "viewOnceMessage") {
+        msg.message = msg.message[msg.type].message;
+        msg.type = getContentType(msg.message);
+      }
+    }
+    if (msg.type === "viewOnceMessage") {
+      msg.message = msg.message[msg.type].message;
+      msg.type = getContentType(msg.message);
+    }
+    try {
+      msg.mentions = msg.message[msg.type].contextInfo
+        ? msg.message[msg.type].contextInfo.mentionedJid || []
+	: [];
+    } catch {
+      msg.mentions = [];
+    }
+    try {
+      const quoted = msg.message[msg.type].contextInfo;
+      if (quoted.quotedMessage["ephemeralMessage"]) {
+	const tipe = Object.keys(quoted.quotedMessage.ephemeralMessage.message)[0];
+	if (tipe === "viewOnceMessage") {
+	  msg.quoted = {
+	    type: "view_once",
+	    stanzaId: quoted.stanzaId,
+	    sender: conn.decodeJid(quoted.participant),
+	    message: quoted.quotedMessage.ephemeralMessage.message.viewOnceMessage.message,
+	  };
+	} else {
+	  msg.quoted = {
+	    type: "ephemeral",
+	    stanzaId: quoted.stanzaId,
+	    sender: conn.decodeJid(quoted.participant),
+	    message: quoted.quotedMessage.ephemeralMessage.message,
+	  };
+	}
+      } else if (quoted.quotedMessage["viewOnceMessage"]) {
+        msg.quoted = {
+	  type: "view_once",
+	  stanzaId: quoted.stanzaId,
+	  sender: conn.decodeJid(quoted.participant),
+	  message: quoted.quotedMessage.viewOnceMessage.message,
+	};
+      } else {
+	msg.quoted = {
+	  type: "normal",
+	  stanzaId: quoted.stanzaId,
+	  sender: conn.decodeJid(quoted.participant),
+	  message: quoted.quotedMessage,
+	};
+      }
+      msg.quoted.isSelf = msg.quoted.sender === conn.decodeJid(conn.user.id);
+      msg.quoted.mtype = Object.keys(msg.quoted.message).filter(
+        (v) => v.includes("Message") || v.includes("conversation")
+      )[0];
+      msg.quoted.text =
+        msg.quoted.message[msg.quoted.mtype].text ||
+        msg.quoted.message[msg.quoted.mtype].description ||
+	msg.quoted.message[msg.quoted.mtype].caption ||
+	(msg.quoted.mtype == "templateButtonReplyMessage" &&
+	msg.quoted.message[msg.quoted.mtype].hydratedTemplate["hydratedContentText"]) ||
+        msg.quoted.message[msg.quoted.mtype] ||
+        "";
+      msg.quoted.key = {
+	id: msg.quoted.stanzaId,
+	fromMe: msg.quoted.isSelf,
+	remoteJid: msg.from,
+      };
+      msg.quoted.delete = () => conn.sendMessage(msg.from, { delete: msg.quoted.key });
+      msg.quoted.download = (pathFile) => downloadMedia(msg.quoted.message, pathFile);
+    } catch (e) {
+      msg.quoted = null;
+    }
+    try {
+      msg.body =
+	msg.message.conversation ||
+	msg.message[msg.type].text ||
+	msg.message[msg.type].caption ||
+	(msg.type === "listResponseMessage" && msg.message[msg.type].singleSelectReply.selectedRowId) ||
+	(msg.type === "buttonsResponseMessage" &&
+	msg.message[msg.type].selectedButtonId &&
+	msg.message[msg.type].selectedButtonId) ||
+	(msg.type === "templateButtonReplyMessage" && msg.message[msg.type].selectedId) ||
+	"";
+    } catch {
+      msg.body = "";
+    }
+    msg.getQuotedObj = msg.getQuotedMessage = async () => {
+      if (!msg.quoted.stanzaId) return false;
+      let q = await store.loadMessage(msg.from, msg.quoted.stanzaId, conn);
+      return serialize(q, conn);
+    }
+    msg.reply = async (text, opt = { isTranslate: true }, from) =>
+    conn.sendMessage(
+      msg.from ? msg.from : from, {
+      text: require("util").format(text),
+      mentions: opt.withTag
+	? [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + "@s.whatsapp.net")
+	: [],
+      ...opt,
+    }, { ...opt, quoted: msg })
+    msg.download = (pathFile) => downloadMedia(msg.message, pathFile);
+  }
+  return msg;
+}
+
+module.exports = { serialize, downloadMedia }
