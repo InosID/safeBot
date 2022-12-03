58b2958ca2b77f7b9e42da91a846d123b542fa9d
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-03 21:20:03 +0700

    Create language.js

---
+++ app/command/configuration/language.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "language",
+  category: "configuration",
+  alias: ["lang"],
+  use: "[language]",
+  isAdmin: true,
+  async run({ msg, conn }, { args }) {
+    let { sender } = msg
+    let user = db.data.users[sender]
+    let languages = (args[0] || '').toLowerCase()
+    switch(languages) {
+      case "english":
+        if (user.language == require('./language/english')) return msg.reply("You are already in English")
+        user.language = require('./language/english')
+        msg.reply('Successfully changed the language to English')
+      break
+      case "indonesia":
+      break
+    }
+  }
+}
