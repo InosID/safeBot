5734e9acc8dbddc95cada754651d21c4d3186b81
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-03 16:28:56 +0700

    Update english.js

--- app/language/english.js
+++ app/language/english.js
@@ -1,3 +1,19 @@
 exports.cooldown = (cooldown) => {
   return `Cooldown ${cooldown} second, wait and try again`
 }
+
+exports.onlyOwner = () => {
+  return `This command can only be used by the owner`
+}
+exports.onlyGroup = () => {
+  return `This command can only be used in groups`
+}
+exports.onlyGroupAdmin = () => {
+  return `This command can only be used by group admins`
+}
+exports.onlyBotAdmin = () => {
+  return `Make Bot as admin to use command`
+}
+exports.onlyPrivate = () => {
+  return `This command can only be used in private chat`
+}
