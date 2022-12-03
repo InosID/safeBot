d1b0fa941b1559369b359b7de0856dd02743d560
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-03 16:31:12 +0700

    Update indonesia.js

--- app/language/indonesia.js
+++ app/language/indonesia.js
@@ -1,3 +1,19 @@
 exports.cooldown = (cooldown) => {
   return `Cooldown ${cooldown} detik, tunggu dan coba lagi`
 }
+
+exports.onlyOwner = () => {
+  return `Perintah ini hanya dapat digunakan oleh owner`
+}
+exports.onlyGroup = () => {
+  return `Perintah ini hanya dapat digunakan di dalam grup`
+}
+exports.onlyGroupAdmin = () => {
+  return `Perintah ini hanya dapat digunakan oleh admin grup`
+}
+exports.onlyBotAdmin = () => {
+  return `Jadikan Bot sebagai admin untuk menggunakan perintah`
+}
+exports.onlyPrivate = () => {
+  return `Perintah ini hanya dapat digunakan dalam chat pribadi`
+}
