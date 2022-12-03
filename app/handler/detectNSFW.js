d16e20f1e194b78a9ee07d7e53b8ec1576deffb2
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-04 06:57:19 +0700

    Update detectNSFW.js

--- app/handler/detectNSFW.js
+++ app/handler/detectNSFW.js
@@ -1,1 +1,33 @@
+let { webp2png } = require('../../lib/webp2')
+let tf = require('@tensorflow/tfjs-node')
+let nsfw = require('nsfwjs')
+let axios = require('axios')
 
+/* Anti NSFW [ BETA ] by @moo-d & @mrfzvx12 */
+
+module.exports = async (msg, conn) => {
+  const { isGroup, from, sender, body, type } = msg
+  let isAntiNSFW = isGroup ? db.data.group[from].antinsfw : false 
+  let isImage = type === 'imageMessage'
+  let isSticker = type === 'stickerMessage' 
+  let isMedia = isImage || isSticker
+  if (isGroup && isAntiNSFW) {
+    if (isMedia) {
+      let imgURL = isMedia ? await webp2png(await msg.download()) : ''
+      let pic = await axios.get(imgURL, {
+        responseType: 'arraybuffer',
+      })
+      const model = await nsfw.load()
+      let image = tf.node.decodeImage(pic.data, 3)
+      let predictions = await model.classify(image)
+      let typeNSFW = predictions[0].className == 'Porn' ? 'Porn' : '' || predictions[0].className == 'Hentai' ? 'Hentai' : ''
+      let maxDet = isImage ? 0.9 : '' || isSticker ? 0.75 : ''
+      if (await predictions[0].probability > maxDet && (typeNSFW == 'Porn' || typeNSFW == 'Hentai')) {
+        await conn.sendMessage(msg.from, { delete: msg.key })
+        await conn.sendMessage(msg.from, { delete: msg.key })
+        require('delay')(2000) 
+        conn.sendMessage(msg.from, { text: `@${msg.sender.split('@')[0]} You are not allowed to post ${typeNSFW} here`, mentions: [msg.sender] })
+      } else console.log('Neutral')
+    }
+  }
+}
