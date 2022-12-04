35925bdfc29c0f385963db2f6dd07741fed8a878
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-04 07:42:29 +0700

    Update antinsfw.js

--- app/command/group/antinsfw.js
+++ app/command/group/antinsfw.js
@@ -1,1 +1,34 @@
-
+module.exports = {
+  name: "antinsfw",
+  category: "group",
+  use: "on / off",
+  desc: "Enable anti nsfw in the group",
+  isGroup: true,
+  isAdmin: true,
+  isBotAdmin: true,
+  async run({ msg, conn }, { q }) {
+    text = q
+    if (text === 'on') {
+      if (db.data.group[msg.from].antinsfw === true) return msg.reply('anti nsfw is active')
+      db.data.group[msg.from].antinsfw = true
+      msg.reply(`Successfully activated anti nsfw. Now 18+ photos and stickers will be automatically removed.`)
+    } else if (text === 'off') {
+      if (db.data.group[msg.from].antinsfw === false) return msg.reply('anti nsfw is deactivated')
+      db.data.group[msg.from].antinsfw = false
+      msg.reply(`Successfully disable anti nsfw`)
+    } else {
+      let buttons = [{ 
+	buttonId: `antinsfw on`, buttonText: { displayText: 'On'}, type: 1
+      }, {
+	buttonId: `antinsfw off`, buttonText: { displayText: 'Off'}, type: 1
+      }]
+      let buttonMessage = {
+        text: `*ANTI NSFW [BETA]*\n*Status:* ${db.data.group[msg.from].antinsfw ? 'Active' : 'Not active'}`,
+	footer: ' ',
+	buttons: buttons,
+	headerType: 4
+      }
+      conn.sendMessage(msg.from, buttonMessage)
+    }
+  }
+}
