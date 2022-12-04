9437e6b6204e75ee62deeb13a7c5a021646b86be
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-05 05:58:47 +0700

    Update onlymember.js

--- app/command/configuration/onlymember.js
+++ app/command/configuration/onlymember.js
@@ -19,6 +19,19 @@ module.exports = {
         group.onlymember = false
         msg.reply(msg.successConfig('All participants'))
       break
+    default:
+      let buttons = [{ 
+	buttonId: `onlymember on`, buttonText: { displayText: 'On'}, type: 1
+      }, {
+	buttonId: `onlymember off`, buttonText: { displayText: 'Off'}, type: 1
+      }]
+      let buttonMessage = {
+        text: `*ONLY MEMBER*\n*Current:* ${group.onlymember ? 'On' : 'Off'}`,
+	footer: ' ',
+	buttons: buttons,
+	headerType: 4
+      }
+      conn.sendMessage(msg.from, buttonMessage)
     }
   }
 }
