8ac47e7dbc0b240be02f2eda7bbe9cf5a440dcb6
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-05 05:51:51 +0700

    Update onlymember.js

--- app/command/configuration/onlymember.js
+++ app/command/configuration/onlymember.js
@@ -1,21 +1,24 @@
-/*module.exports = {
+module.exports = {
   name: "onlymember",
   alias: ["onlymem"],
   category: "configuration",
   use: "yes / no",
   isGroup: true,
+  isAdmin: true,
   async run({ msg, conn }, { args }) {
     let om = (args[0] || "").toLowerCase()
     let group = db.data.group[msg.from]
     switch(om) {
       case 'yes':
-        if (group.onlymember) return msg.reply()
+        if (group.onlymember) return msg.reply(lang.isConfig('Only member'))
         group.onlymember = true
-        msg.reply(msg.done())
+        msg.reply(msg.successConfig('Only Member'))
       break
       case 'no':
-        
+        if (group.onlymember) return msg.reply(lang.isConfig('All participants'))
+        group.onlymember = false
+        msg.reply(msg.successConfig('All participants'))
       break
     }
   }
-}*/
+}
