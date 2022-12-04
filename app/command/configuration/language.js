b6afefaffae32405d4054c3b07c90b6855138cdf
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-12-04 20:41:12 +0700

    Update language.js

--- app/command/configuration/language.js
+++ app/command/configuration/language.js
@@ -1,4 +1,4 @@
-/*module.exports = {
+module.exports = {
   name: "language",
   category: "configuration",
   alias: ["lang"],
@@ -11,13 +11,15 @@
     let languages = (args[0] || '').toLowerCase()
     switch(languages) {
       case "english":
-        if (user.language == require('./language/english')) return msg.reply("You are already in English")
-        user.language = require('./language/english')
+        if (user.language == 'english') return msg.reply("You are already in English")
+        user.language = 'english'
         msg.reply('Successfully changed the language to English')
       break
       case "indonesia":
+        if (user.language == 'indonesia') return msg.reply("Kamu sudah berbahasa Indonesia")
+        user.language = 'indonesia'
+        msg.reply('Berhasil mengganti ke bahasa Indonesia')
       break
     }
   }
 }
-*/
