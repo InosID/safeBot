53d8fa159e42ca375099f28f4b52ebfbbeb63bc5
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-09 07:15:10 +0700

    Update welcome.js

--- app/canvas/welcome.js
+++ app/canvas/welcome.js
@@ -1,1 +1,26 @@
+let sizeOf = require('image-size')
+let axios = require('axios')
+let Canvas = require('canvas')
+let jimp = require('jimp')
+let gifframes = require('gif-frames')
+let GIFEncoder = require('gif-encoder-2')
 
+Canvas.registerFont(require('@canvas-fonts/arial-bold'), {
+  family: 'Arial-Bold'
+})
+
+class Welcomer {
+  constructor({
+    background,
+    name,
+    discriminator,
+    avatar,
+    gif,
+    layer,
+    blur,
+    delay,
+    frame_limit
+  } = {}) {}
+}
+
+module.exports = Welcomer
