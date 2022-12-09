aaafe865880c042855f60cd37ea63a92c9b0655e
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-09 16:02:07 +0700

    Update welcome.js

--- app/canvas/welcome.js
+++ app/canvas/welcome.js
@@ -154,7 +154,80 @@ class Welcomer {
   }
 
   /* Method to generate static image */
-  async _generateImage() {}
+  async _generateImage() {
+    const img = await this._getImageSize(this.background)
+
+    const canvas = Canvas.createCanvas(700, 250)
+    const ctx = canvas.getContext('2d')
+
+    const scale = Math.max(canvas.width / img.width, canvas.height / img.height)
+    const x = (canvas.width / 2) - (img.width / 2) * scale
+    const y = (canvas.height / 2) - (img.height / 2) * scale
+
+    let background = await jimp.read(this.background)
+    const layer = await Canvas.loadImage(this.layer)
+
+    if (this.blur) background.blur(this.blur)
+    background = await background.getBufferAsync('image/png')
+
+    ctx.drawImage(await Canvas.loadImage(background), x, y, img.width * scale, img.height * scale)
+
+    ctx.strokeRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(layer, 0, 0, canvas.width, canvas.height)
+
+    const name = this.name.length > 12 ? this.name.substring(0, 12) + '...' : this.name
+
+    ctx.font = 'bold 36px Arial Bold'
+    ctx.fillStyle = '#FFFFFF'
+    ctx.textAlign = 'start'
+    ctx.strokeStyle = '#f5f5f5'
+    ctx.fillText(`${name}`, 278, 113)
+
+    ctx.strokeText(`${name}`, 278, 113)
+
+    ctx.font = 'bold 25px Arial Bold'
+    ctx.fillStyle = '#FFFFFF'
+
+    ctx.fillText(`${this.memberCount}`, 278, 162.5)
+
+    let avatar = await jimp.read(this.avatar)
+
+    avatar.resize(1024, 1024).circle()
+    avatar = await avatar.getBufferAsync('image/png')
+    avatar = await Canvas.loadImage(avatar)
+
+    ctx.drawImage(avatar, 72, 48, 150, 150)
+
+    return canvas.toBuffer()
+  }
+
+  /* Generate image with saved settings */
+  async generate() {
+    if (!this.gif) return this._generateImage()
+
+    const firstframe = await gifFrames({
+      url: this.background,
+      frames: 0
+    })
+    const cumulative = firstframe[0].frameInfo.disposal !== 1 ? false : true
+
+    let data = await gifFrames({
+      url: this.background,
+      frames: 'all',
+      cumulative
+    })
+    if (data.length >= this.frame_limit) data = data.slice(0, this.frame_limit)
+
+    const encoder = new GIFEncoder(700, 250)
+    encoder.start()
+    encoder.setDelay(this.delay)
+
+    const frames = await Promise.all(data.map(x => this._renderFrame(x)))
+    for (let frame of frames) encoder.addFrame(frame)
+
+    encoder.finish()
+    return encoder.out.getData()
+  }
 }
 
 module.exports = Welcomer
