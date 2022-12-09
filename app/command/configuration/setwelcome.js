a06bf3a3e12d8a8323b42ad45f8f6756a1f8e71f
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-09 20:24:05 +0700

    Create setwelcome.js

---
+++ app/command/configuration/setwelcome.js
@@ -0,0 +1,10 @@
+/*module.exports = {
+  name: "setwelcome",
+  category: "configuration",
+  use: "[optional]",
+  isGroup: true,
+  isAdmin: true,
+  async run({ msg, conn }, { args }) {
+    
+  }
+}*/
