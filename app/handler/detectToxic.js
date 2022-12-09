f345d3c529d3b6e7a8893ed9593d7a891a482e83
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-09 20:50:04 +0700

    Create detectToxic.js

---
+++ app/handler/detectToxic.js
@@ -0,0 +1,6 @@
+/*module.exports = async (msg, conn) => {
+  const { isGroup, from, sender, body, type } = msg
+  let isAntiToxic = isGroup ? db.data.group[from].antitoxic : false
+  let toxicList = db.data.group[from].toxiclist
+  if (isAntiToxic) {}
+}*/
