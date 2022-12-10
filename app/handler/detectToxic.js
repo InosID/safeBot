8938e1d4c85ab228af4e3fc9e20bd020139ee644
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-10 09:56:44 +0700

    Update detectToxic.js

--- app/handler/detectToxic.js
+++ app/handler/detectToxic.js
@@ -1,6 +1,10 @@
-/*module.exports = async (msg, conn) => {
+module.exports = async (msg, conn) => {
   const { isGroup, from, sender, body, type } = msg
   let isAntiToxic = isGroup ? db.data.group[from].antitoxic : false
   let toxicList = db.data.group[from].toxiclist
-  if (isAntiToxic) {}
-}*/
+  if (isGroup && isAntiToxic) {
+    if (toxicList.some(v => body.includes(v))) {
+      console.log('Detected toxic')
+    }
+  }
+}
