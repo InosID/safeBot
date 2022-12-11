65503aaf0207674c23410940e56fb6e02a7ad2f1
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-12 05:13:52 +0700

    Update detectBadWord.js

--- app/handler/detectBadWord.js
+++ app/handler/detectBadWord.js
@@ -12,6 +12,15 @@ module.exports = async (msg, conn) => {
     if (confirm) {
       await conn.sendMessage(msg.from, { delete: msg.key })
       conn.sendMessage(from, { text: `@${sender.split('@')[0]} ` + lang.detectedToxic(), mentions: [sender] })
+      if (group.autokick) {
+        db.data.users[sender].warn += 1
+        if (!oAdmin && warn >= maxwarn) {
+          msg.reply(lang.warnkick())
+          require('delay')(3000)
+          await conn.groupParticipantsUpdate(msg.from, [sender], "remove")
+          db.data.users[sender] = 0
+        }
+      }
     }
   }
 }
