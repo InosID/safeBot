e5ad0495fb9fab5b02f57968d47476992ea2c3e9
ナズワ サルサビラ <75057738+moo-d@users.noreply.github.com>
2022-12-12 17:47:24 +0700

    Create badword.js

---
+++ app/command/configuration/badword.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: "badword",
+  category: "configuration",
+  use: "[add/remove/reset]",
+  isGroup: true,
+  isAdmin: true,
+  async run({ msg, conn }, { args }) {
+    let select = (args[0] || '').toLowerCase()
+    let text = msg.body.trim().split(/ +/).slice(2).join(" ")
+    let group = db.data.group[msg.from]
+    let badwordList = group.toxiclist
+    let listed = badwordList.findIndex(e => e === text)
+    switch(select) {
+      case "add":
+        if (args.length < 2) return msg.reply(lang.needText())
+        if (listed == -1) {
+          badwordList.push(text)
+          msg.reply(lang.added(text))
+        } else msg.reply(lang.available(text))
+      break
+      case "remove":
+        if (args.length < 2) return msg.reply(lang.needText())
+        if (!listed == -1) {
+          badwordList.splice(badwordList.indexOf(text), 1)
+          msg.reply(removed(text))
+        } else msg.reply(lang.notAvailable(text))
+      break
+      case "reset":
+        delete badwordList
+        msg.reply(lang.done())
+      break
+    default:
+      msg.reply(`Example:\nbadword add f***\nbadword remove f***\nbadword reset`)
+    }
+  }
+}
